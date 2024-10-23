30121b3023d70010da6661e846552b3aea09313e
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-23 16:11:52 +0900

    Update index.js

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -14,6 +14,7 @@ import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 
 // chapter_03/Library
@@ -118,10 +119,20 @@ import ProfileCard from './chapter_13/ProfileCard';
 
 
 // chapter_13/ProfileCard
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <ProfileCard />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// chapter_14/DarkOrLight
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>,
   document.getElementById('root')
 );
