a33fb13dae0896fb8e3563e0cd9a6b9538add0c8
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-24 22:37:32 +0900

    Update App.js

--- rock-scissor-paper/src/App.js
+++ rock-scissor-paper/src/App.js
@@ -1,12 +1,43 @@
-import HandButton from "./HandButton";
+import Button from './Button';
+import HandButton from './HandButton';
+import HandIcon from './HandIcon';
+import { compareHand, generateRandomHand } from './utils';
+
+function getResult(me, other) {
+  const comparison = compareHand(me, other);
+  if (comparison > 0) return '승리';
+  if (comparison < 0) return '패배';
+  return '무승부';
+}
 
 function App() {
-  const handleClick = (value) => console.log(value);
+  // hand와 otherHand를 state로 바꿔 주세요
+  const hand = 'rock';
+  const otherHand = 'scissor';
+
+  const handleButtonClick = (nextHand) => {
+    // hand의 값을 nextHand 로 바꿔 주세요
+    // otherHand의 값을 generateRandomHand()의 리턴 값으로 바꿔주세요
+  };
+
+  const handleClearClick = () => {
+    // hand와 otherHand의 값을 'rock' 으로 바꿔주세요
+  };
+
   return (
     <div>
-      <HandButton value="rock" onClick={handleClick} />
-      <HandButton value="scissor" onClick={handleClick} />
-      <HandButton value="paper" onClick={handleClick} />
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        <HandIcon value={hand} />
+        VS
+        <HandIcon value={otherHand} />
+      </div>
+      <div>
+        <HandButton value="rock" onClick={handleButtonClick} />
+        <HandButton value="scissor" onClick={handleButtonClick} />
+        <HandButton value="paper" onClick={handleButtonClick} />
+      </div>
     </div>
   );
 }
