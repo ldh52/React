f00d1b2d9fc3274f748a4cad38342651d95646b2
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-25 04:53:37 +0900

    Update App.js

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
+import Board from './Board';
 import Button from './Button';
-import Dice from './Dice';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -10,33 +10,37 @@ function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0);
   const [gameHistory, setGameHistory] = useState([]);
+  const [otherNum, setOtherNum] = useState(1);
+  const [otherSum, setOtherSum] = useState(0);
+  const [otherGameHistory, setOtherGameHistory] = useState([]);
 
   const handleRollClick = () => {
     const nextNum = random(6);
+    const nextOtherNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum);
     setGameHistory([...gameHistory, nextNum]);
+    setOtherNum(nextOtherNum);
+    setOtherSum(otherSum + nextOtherNum);
+    setOtherGameHistory([...otherGameHistory, nextOtherNum]);
   };
 
   const handleClearClick = () => {
     setNum(1);
     setSum(0);
     setGameHistory([]);
+    setOtherNum(1);
+    setOtherSum(0);
+    setOtherGameHistory([]);
   };
 
   return (
     <div>
+      <Button onClick={handleRollClick}>던지기</Button>
+      <Button onClick={handleClearClick}>처음부터</Button>
       <div>
-        <Button onClick={handleRollClick}>던지기</Button>
-        <Button onClick={handleClearClick}>처음부터</Button>
-      </div>
-      <div>
-        <h2>나</h2>
-        <Dice color='blue' num={num} />
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <h2>기록</h2>
-        {gameHistory.join(', ')}
+        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} />
+        <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory} />
       </div>
     </div>
   );
