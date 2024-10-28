c6493e1486115fbca8908fabb5f661e149a323fd
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-28 17:06:35 +0900

    하나의 state로 폼 구현하기

--- foodit/src/components/FoodForm.js
+++ foodit/src/components/FoodForm.js
@@ -1,29 +1,36 @@
 import { useState } from "react";
 
 function FoodForm() {
-  const [title, setTitle] = useState("");
-  const [calorie, setCalorie] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    calorie: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => setTitle(e.target.value);
-
-  const handleCalorieChange = (e) => {
-    const nextCalorie = Number(e.target.value) || 0;
-    setCalorie(nextCalorie);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
 
-  const handleContentChange = (e) => setContent(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
 
   return (
-    <form>
-      <input name="title" value={title} onChange={handleTitleChange} />
+    <form onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
       <input
         type="number"
         name="calorie"
-        value={calorie}
-        onChange={handleCalorieChange}
+        value={values.calorie}
+        onChange={handleChange}
       />
-      <input name="content" value={content} onChange={handleContentChange} />
+      <input name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">확인</button>
     </form>
   );
 }
